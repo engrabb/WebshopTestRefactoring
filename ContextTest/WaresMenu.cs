4f64dcc63014357ade94ac79eabd78a10cc0fcdd
Erik W <erik.h.werner@gmail.com>
2023-04-11 21:16:34 +0200

    Changes

--- ContextTest/WaresMenu.cs
+++ ContextTest/WaresMenu.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebshopTest.ContextTest;
-using WebshopTest.Interfaces;
 namespace WebshopTest
 {
     public class WaresMenu : IWebshopSystem
